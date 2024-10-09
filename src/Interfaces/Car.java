69a4756c0ca6825c602d4b98703b9862cf7a3120
ddevgeek <diclanjonathan@gmail.com>
2024-10-09 06:38:32 +0300

    refactor

--- src/Interfaces/Car.java
+++ src/Interfaces/Car.java
@@ -4,6 +4,7 @@ public interface Car {
     void run();
     void stop();
     void repair();
+    double sell();
 }
 
 //it should have run, stop, repair functions.
--- src/MonOct_7.java
+++ src/MonOct_7.java
@@ -2,10 +2,10 @@ import Interfaces.Car;
 
 public class MonOct_7 {
     public static void main(String[] args) {
-        FourDoorCar toyota1 = new FourDoorToyota("Toyota", "Gray", "330 mph");
-        FourDoorCar fourDoorBM = new FourDoorBMW("BMW", "DarkRed", "200 mph");
-        FourDoorCar fourDoorFord = new FourDoorFord("BMW", "DarkRed", "200 mph");
-        FourDoorCar fourDoorHyundai = new FourDoorHyundai("BMW", "DarkRed", "300 mph");
+        FourDoorCar toyota1 = new FourDoorToyota("Toyota", "Gray", "330 mph", 2000, 30, 180);
+        FourDoorCar fourDoorBM = new FourDoorBMW("BMW", "DarkRed", "200 mph", 2000, 30, 100);
+        FourDoorCar fourDoorFord = new FourDoorFord("BMW", "DarkRed", "200 mph", 2000, 30, 7);
+        FourDoorCar fourDoorHyundai = new FourDoorHyundai("BMW", "DarkRed", "300 mph", 2000, 30, 180);
 
         System.out.println();
         System.out.println("======== Four Door Cars ============");
@@ -13,10 +13,10 @@ public class MonOct_7 {
         toyota1.stop();
         toyota1.repair();
 
-        Car toyota2 = new TwoDoorToyota("Toyota", "Gray", "330 mph");
-        Car twoDoorBM2 = new TwoDoorBMW("BMW", "DarkRed", "200 mph");
-        Car twoDoorFord2 = new TwoDoorFord("BMW", "DarkRed", "200 mph");
-        Car twoDoorHyundai2 = new TwoDoorHyundai("BMW", "DarkRed", "300 mph");
+        Car toyota2 = new TwoDoorToyota("Toyota", "Gray", "330 mph", 2000, 30, 100);
+        Car twoDoorBM2 = new TwoDoorBMW("BMW", "DarkRed", "200 mph", 2000, 30, 80);
+        Car twoDoorFord2 = new TwoDoorFord("BMW", "DarkRed", "200 mph", 2000, 30, 75);
+        Car twoDoorHyundai2 = new TwoDoorHyundai("BMW", "DarkRed", "300 mph", 2000, 30, 100);
 
         System.out.println();
         System.out.println("======== Two Door Cars ============");
@@ -34,11 +34,18 @@ abstract class TwoDoorCar implements Car {
     private String carBrand;
     private String carColor;
     private String carSpeed;
+    private int carPower;
+    private int year;
+    private int mileage;
+    private int power;
 
-    public TwoDoorCar(String carBrand, String carColor, String carSpeed) {
+    public TwoDoorCar(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
         this.carBrand = carBrand;
         this.carColor = carColor;
         this.carSpeed = carSpeed;
+        this.year = year;
+        this.mileage = mileage;
+        this.power = power;
     }
 
     public String getCarBrand() {
@@ -65,6 +72,18 @@ abstract class TwoDoorCar implements Car {
         this.carSpeed = carSpeed;
     }
 
+    public int getCarPower() {
+        return carPower;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
     @Override
     public void run() {
         System.out.println("The car is running");
@@ -81,19 +100,24 @@ abstract class TwoDoorCar implements Car {
     }
 
     @Override
-    public String toString() {
-        return "TwoDoorCar{" +
-                "carBrand='" + carBrand + '\'' +
-                ", carColor='" + carColor + '\'' +
-                ", carSpeed='" + carSpeed + '\'' +
-                '}';
+    public double sell() {
+        return 0;
     }
+
+//    @Override
+//    public String toString() {
+//        return "TwoDoorCar{" +
+//                "carBrand='" + carBrand + '\'' +
+//                ", carColor='" + carColor + '\'' +
+//                ", carSpeed='" + carSpeed + '\'' +
+//                '}';
+//    }
 }
 
 abstract class FourDoorCar extends TwoDoorCar {
 
-    public FourDoorCar(String carBrand, String carColor, String carSpeed) {
-        super(carBrand, carColor, carSpeed);
+    public FourDoorCar(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
+        super(carBrand, carColor, carSpeed, year, mileage, power);
     }
 
     @Override
@@ -111,34 +135,59 @@ abstract class FourDoorCar extends TwoDoorCar {
     public void repair() {
         System.out.println(this.getCarBrand() + " is being repaired");
     }
+
+    @Override
+    public double sell() {
+        return 12000 + (( getYear() == 0 ? 0.001 : getYear() / getMileage() == 0 ? 0.0001 : getMileage() ) * getCarPower());
+    }
 }
 
 class FourDoorToyota extends FourDoorCar {
 
-    public FourDoorToyota(String carBrand, String carColor, String carSpeed) {
-        super(carBrand, carColor, carSpeed);
+    public FourDoorToyota(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
+        super(carBrand, carColor, carSpeed, year, mileage, power);
+    }
+
+    @Override
+    public String toString() {
+        return this.getCarBrand() + " " + this.getCarSpeed() + " " + this.getCarColor();
     }
 
 }
 
 class FourDoorBMW extends FourDoorCar {
 
-    public FourDoorBMW(String carBrand, String carColor, String carSpeed) {
-        super(carBrand, carColor, carSpeed);
+    public FourDoorBMW(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
+        super(carBrand, carColor, carSpeed, year, mileage, power);
+    }
+
+    @Override
+    public String toString() {
+        return this.getCarBrand() + " " + this.getCarSpeed() + " " + this.getCarColor();
     }
 }
 
 class FourDoorFord extends FourDoorCar {
 
-    public FourDoorFord(String carBrand, String carColor, String carSpeed) {
-        super(carBrand, carColor, carSpeed);
+    public FourDoorFord(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
+        super(carBrand, carColor, carSpeed, year, mileage, power);
+    }
+
+    @Override
+    public String toString() {
+        return this.getCarBrand() + " " + this.getCarSpeed() + " " + this.getCarColor();
     }
 }
 
 class FourDoorHyundai extends FourDoorCar {
 
-    public FourDoorHyundai(String carBrand, String carColor, String carSpeed) {
-        super(carBrand, carColor, carSpeed);
+    public FourDoorHyundai(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
+        super(carBrand, carColor, carSpeed, year, mileage, power);
+    }
+
+    @Override
+    public String toString() {
+        return this.getCarBrand() + " " + this.getCarSpeed() + " " + this.getCarColor();
     }
 }
 
@@ -146,29 +195,49 @@ class FourDoorHyundai extends FourDoorCar {
 
 class TwoDoorToyota extends TwoDoorCar {
 
-    public TwoDoorToyota(String carBrand, String carColor, String carSpeed) {
-        super(carBrand, carColor, carSpeed);
+    public TwoDoorToyota(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
+        super(carBrand, carColor, carSpeed, year, mileage, power);
+    }
+
+    @Override
+    public String toString() {
+        return this.getCarBrand() + " " + this.getCarSpeed() + " " + this.getCarColor();
     }
 }
 
 class TwoDoorBMW extends FourDoorCar {
 
-    public TwoDoorBMW(String carBrand, String carColor, String carSpeed) {
-        super(carBrand, carColor, carSpeed);
+    public TwoDoorBMW(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
+        super(carBrand, carColor, carSpeed, year, mileage, power);
+    }
+
+    @Override
+    public String toString() {
+        return this.getCarBrand() + " " + this.getCarSpeed() + " " + this.getCarColor();
     }
 }
 
 class TwoDoorFord extends FourDoorCar {
 
-    public TwoDoorFord(String carBrand, String carColor, String carSpeed) {
-        super(carBrand, carColor, carSpeed);
+    public TwoDoorFord(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
+        super(carBrand, carColor, carSpeed, year, mileage, power);
+    }
+
+    @Override
+    public String toString() {
+        return this.getCarBrand() + " " + this.getCarSpeed() + " " + this.getCarColor();
     }
 }
 
 class TwoDoorHyundai extends FourDoorCar {
 
-    public TwoDoorHyundai(String carBrand, String carColor, String carSpeed) {
-        super(carBrand, carColor, carSpeed);
+    public TwoDoorHyundai(String carBrand, String carColor, String carSpeed, int year, int mileage, int power) {
+        super(carBrand, carColor, carSpeed, year, mileage, power);
+    }
+
+    @Override
+    public String toString() {
+        return this.getCarBrand() + " " + this.getCarSpeed() + " " + this.getCarColor();
     }
 }
 
